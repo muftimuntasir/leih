3a8caa641a726729285d36c2b733229296511b70
kazi <kazialamin109@gmail.com>
2019-07-06 14:37:18 +0600

    doctors nid updated

--- Doctors/doctors.py
+++ Doctors/doctors.py
@@ -13,7 +13,8 @@ class leih(osv.osv):
         'name': fields.char("Doctor Name",required=True),
         'department':fields.char('Department',required=True),
         'designation':fields.char('Designation',required=True),
-        'status': fields.selection([('a', 'In house'), ('b', 'Consoled'),('c','Prt_time'),('d','Out_side')], string='Status', default='a')
+        'status': fields.selection([('a', 'In house'), ('b', 'Consoled'),('c','Prt_time'),('d','Out_side')], string='Status', default='a'),
+        'nid':fields.integer("NID")
 
 
     }
