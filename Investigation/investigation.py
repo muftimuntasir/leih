f99709ba680503ed392e7e78af5695390f4e0fba
kazi <kazialamin109@gmail.com>
2019-07-18 13:19:20 +0600

    check

--- Investigation/investigation.py
+++ Investigation/investigation.py
@@ -49,16 +49,16 @@ class test_information(osv.osv):
 
 
 
-    def _amount_all(self, cr, uid, ids, field_name, arg, context=None):
-        cur_obj = self.pool.get('leih.investigation')
-        res = {}
-        for record in self.browse(cr, uid, ids, context=context):
-            rate=record.price
-            discount=record.discount
-            interst_amount=int(discount)*int(rate)/100
-            total_amount=int(rate)-interst_amount
-            res[record.id]=total_amount
-        return res
+    # def _amount_all(self, cr, uid, ids, field_name, arg, context=None):
+    #     cur_obj = self.pool.get('leih.investigation')
+    #     res = {}
+    #     for record in self.browse(cr, uid, ids, context=context):
+    #         rate=record.price
+    #         discount=record.discount
+    #         interst_amount=int(discount)*int(rate)/100
+    #         total_amount=int(rate)-interst_amount
+    #         res[record.id]=total_amount
+    #     return res
 
 
 
@@ -69,20 +69,30 @@ class test_information(osv.osv):
         # 'currency_id': fields.related('pricelist_id', 'currency_id', type="many2one", relation="res.currency",
         #                               string="Currency", readonly=True, required=True),
         # 'price_subtotal': fields.function(_amount_line, string='Subtotal', digits_compute=dp.get_precision('Account')),
-        'price': fields.char("Price"),
-        'discount': fields.char("Discount"),
-        'total_amount': fields.function(_amount_all, string="Total Amount"),
+        'price': fields.integer("Price"),
+        'discount': fields.integer("Discount"),
+        'total_amount': fields.integer("Total Amount"),
 
     }
 
     def onchange_test(self,cr,uid,ids,name,context=None):
         tests = {'values': {}}
         dep_object = self.pool.get('leih.testentry').browse(cr, uid, name, context=None)
-        abc = {'price': dep_object.rate}
+        abc = {'price': dep_object.rate,'total_amount':dep_object.rate}
         tests['value'] = abc
         # import pdb
         # pdb.set_trace()
         return tests
+
+    def onchange_discount(self,cr,uid,ids,name,discount,context=None):
+        tests = {'values': {}}
+        dep_object = self.pool.get('leih.testentry').browse(cr, uid, name, context=None)
+        abc = {'total_amount':round(dep_object.rate-(dep_object.rate* discount/100))}
+        tests['value'] = abc
+        # import pdb
+        # pdb.set_trace()
+        return tests
+
 
 
     # def onchange_tamount(self,cr,uid,ids,name,context=None):
