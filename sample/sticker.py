a24c6cf64e7626bb16602f57daac24a017cb55ac
kazi <kazialamin109@gmail.com>
2019-10-01 10:39:23 +0600

    kk

--- sample/sticker.py
+++ sample/sticker.py
@@ -14,8 +14,8 @@ class sample(osv.osv):
     _columns = {
         'sample_id':fields.integer("ID"),
         'bill_register_id':fields.many2one('bill.register','Bill register Id'),
-        'tests_id':fields.many2one('examination.entry','Test ID'),
-        'department_id':fields.char('diagnosis.department','Department'),
+        'tests_id':fields.char('Test ID'),
+        'department_id':fields.many2one('diagnosis.department','Department'),
         'name': fields.char('Name'),
         'sticker_line_id':fields.one2many('diagnosis.sticker.line','sticker_id','Record Sample')
     }
