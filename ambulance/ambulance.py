02bd570330a3da7eef09f8ae925c1a36d61c2ca8
Mufti <mufti.ahmed@zerogravity.com.bd>
2020-01-07 17:24:03 +0600

    Added xml files and into the menu

--- ambulance/ambulance.py
+++ ambulance/ambulance.py
@@ -22,10 +22,10 @@ class ambulance_booking(osv.osv):
     _columns = {
 
         'booking_type': fields.char("Booking Type",required=True),
-        'name':fields.char('Name',required=True),
-        'patient_id':fields.char('Patient Name',required=True),
+        'name':fields.char('Customer Name',required=True),
+        'patient_id':fields.many2one('patient.info', "Patient Name",required=True),
         'mobile_no': fields.char('Mobile No', required=True),
-        'start_from': fields.char('Start From', required=True),
+        'start_from': fields.char('Start/Pick From', required=True),
         'destination': fields.char('Destination', required=True),
         'amount': fields.float('Amount', required=True),
         'advance_amount': fields.float('Advance Amount', required=True),
@@ -33,11 +33,43 @@ class ambulance_booking(osv.osv):
         'unpaid_amount': fields.float('Unpaid Amount'),
         'grace_time': fields.float('Expected Completion Time', required=True),
         'date': fields.datetime('Booking/Request Date and Time', required=True),
+        'ambulance_id': fields.many2one('ambulance.registration', "Vehicle Name"),
+        'state': fields.selection([
+            ('draft', 'Pending'),
+            ('confirm', 'Confirmed'),
+            ('cancel', 'Cancelled'),
+
+        ], 'Status', readonly=True, copy=False, select=True),
 
             }
 
+    _defaults = {
+        'state': 'draft'
+    }
+
 class ambulance_expense(osv.osv):
     _name = "ambulance.expense"
+
+    _columns = {
+        'ambulance_id': fields.many2one('ambulance.registration', "Vehicle Name", required=True),
+        'fuel_cost': fields.float('Fuel Cost'),
+        'other_cost': fields.float('Other Cost'),
+        'description': fields.text('Reason'),
+        'state': fields.selection([
+            ('draft', 'Pending'),
+            ('confirm', 'Confirmed'),
+            ('cancel', 'Cancelled'),
+
+        ], 'Status', readonly=True, copy=False, select=True),
+
+    }
+
+    _defaults = {
+        'state': 'draft'
+    }
+
+
+
 
 
 
