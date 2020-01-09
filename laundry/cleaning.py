7957d9e099f0d041410f2c808335682b14705b0d
Rony <rtex.mmar@gmail.com>
2020-01-09 15:22:22 +0600

    laundry change

--- laundry/cleaning.py
+++ laundry/cleaning.py
@@ -14,11 +14,28 @@ class laundry_clean(osv.osv):
         'send_date':fields.date("Send Date"),
         'laundry_name':fields.many2one('laundry.laundry',"Laundry Name",required=True),
         'back_date': fields.date("Delivered Date"),
+        'state': fields.selection(
+            [('store', 'Store'), ('sent to laundry', 'Sent to Laundry'), ('received', 'Received'),
+             ('cancelled', 'Cancelled')],
+            'Status', default='sent to laundry', required=True, readonly=True, copy=False,
+        ),
         'product_line':fields.one2many("laundry.clean.line","laundry_clean_id","Clean Product List")
         # 'nid':fields.integer("NID")
 
 
     }
+
+    def receive(self,cr,uid,ids,context=None):
+        if ids is not None:
+            cr.execute("update laundry_clean set state='received' where id=%s", (ids))
+            cr.commit()
+        return True
+
+    def cancel_clean(self,cr,uid,ids,context=None):
+        if ids is not None:
+            cr.execute("update leih_expense set state='cancelled' where id=%s", (ids))
+            cr.commit()
+        return True
 
 class clean_line(osv.osv):
     _name = 'laundry.clean.line'
@@ -29,7 +46,7 @@ class clean_line(osv.osv):
         'laundry_clean_id': fields.many2one('laundry.clean', "Clean"),
         'linen_item': fields.many2one("laundry.product","Linen Item"),
         'quantity': fields.integer("Quantity"),
-        'color': fields.char("Color")
+        'color': fields.char("Color"),
     }
 
 
