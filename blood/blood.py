4275dd0bda1c77ee6563964510b9402132314854
kazi <kazialamin109@gmail.com>
2020-01-09 19:05:56 +0600

    Renamed group to blood group

--- blood/blood.py
+++ blood/blood.py
@@ -6,16 +6,18 @@ class blood_donar(osv.osv):
     _name = "blood.donar"
 
 
+
     _columns = {
         'name':fields.char('Name'),
         'doner_name':fields.char('Donar Name',required=True),
         'mobile_no':fields.char('Mobile No',required=True),
-        'receive_date':fields.date('Received Date',required=True),
         'date':fields.date('Received Date',required=True),
+        'receive_date':fields.date('Received Date'),
         'description':fields.text('Description'),
         'group':fields.char('Blood Group'),
         'cost':fields.float('Cost'),
-        'active': fields.boolean('Available'),
+        'active': fields.boolean('Expired'),
+        'donated': fields.boolean('donated'),
             }
 
 
@@ -31,12 +33,27 @@ class blood_receiver(osv.osv):
         'mobile_no':fields.char('Mobile No',required=True),
         'patient_id': fields.many2one('patient.info', "Patient Name"),
         'description':fields.text('Description'),
-        'group':fields.char('Blood Group'),
+        'blood_group':fields.char('Blood Group'),
         'price':fields.float('Price'),
         'paid_amount':fields.float('Paid Amount'),
         'unpaid_amount':fields.float('Unpaid Amount'),
 
             }
+
+    def create(self, cr, uid, vals, context=None):
+        if context is None:
+            context = {}
+
+
+
+        stored = super(bill_register, self).blood_receiver(cr, uid, vals, context)  # return ID int object
+
+
+
+
+
+        return stored
+
 
 
 
