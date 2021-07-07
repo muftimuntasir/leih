58dd0179a21895cd73a6a1b8df58600b5fdf2e3b
kazi <kazialamin109@gmail.com>
2021-07-07 11:56:14 +0600

    optics form updated

--- optics/optics_sale.py
+++ optics/optics_sale.py
@@ -16,23 +16,18 @@ class optics_sale(osv.osv):
             total_list = []
             for amount in items.optics_sale_line_id:
                 total_list.append(amount.total_amount)
-
             for item in total_list:
                 sum = item + sum
-
                 for record in self.browse(cr, uid, ids, context=context):
                     Percentance_calculation[record.id] = sum
-
         return Percentance_calculation
 
-
     _columns = {
-
         # 'patient_id': fields.char("Patient ID"),
         'name': fields.char("Name"),
         'mobile': fields.char(string="Mobile", readonly=True, store=False),
         'patient_id': fields.char(related='patient_name.patient_id', string="Patient Id", readonly=True),
-        'patient_name': fields.many2one('patient.info', "Patient Name",required=True),
+        'patient_name': fields.many2one('patient.info', "Patient Name", required=True),
         'address': fields.char("Address", store=False),
         'age': fields.char("Age", store=False),
         'sex': fields.char("Sex", store=False),
@@ -49,14 +44,15 @@ class optics_sale(osv.osv):
         'left_eye_cyl_n': fields.char('Left Eye CYL -N'),
         'left_eye_axis_n': fields.char('Left Eye AXIS -N'),
         'delivery_date': fields.date(string="Delivery Date"),
-        'hard_cover':fields.boolean("Cover",default=True),
-        'cell_pad':fields.boolean("Cell Pad",default=True),
-        'frame_id':fields.many2one('product.product','Frame'),
-        'delivery_id':fields.many2one('stock.picking','Delivery Challan'),
-        'price':fields.float('Price'),
+        'hard_cover': fields.boolean("Cover", default=True),
+        'cell_pad': fields.boolean("Cell Pad", default=True),
+        'frame_id': fields.many2one('product.product', 'Frame'),
+        'qty_available':fields.integer("Stock Quantity", readonly=True),
+        'delivery_id': fields.many2one('stock.picking', 'Delivery Challan'),
+        'price': fields.float('Price'),
         'optics_lens_sale_line_id': fields.one2many('optics.lens.sale.line', 'optics_sale_id', 'Lens Entry'),
         'optics_sale_payment_line_id': fields.one2many("optics.sale.payment.line", "optics_sale_payment_line_id",
-                                                         "Bill Register Payment"),
+                                                       "Bill Register Payment"),
         # 'footer_connection': fields.one2many('leih.footer', 'relation', 'Parameters', required=True),
         # 'relation': fields.many2one("leih.investigation"),
         # 'total': fields.float(_totalpayable,string="Total",type='float',store=True),
@@ -77,22 +73,15 @@ class optics_sale(osv.osv):
     }
 
     # if same item exist in line
-
-
     def bill_confirm(self, cr, uid, ids, context=None):
-
         stored_obj = self.browse(cr, uid, [ids[0]], context=context)
         ## Bill Status Will Change
-
-
         stored = int(ids[0])
         if stored_obj.state == 'confirmed':
             raise osv.except_osv(_('Warning!'),
                                  _('Already it is  Confirmed. Yuo can not change.'))
-
         if stored_obj.paid != False:
             for bills_vals in stored_obj:
-
                 mr_value = {
                     'date': stored_obj.date,
                     'optics_sale_id': int(stored),
@@ -108,36 +97,25 @@ class optics_sale(osv.osv):
                 mr_name = 'MR#' + str(mr_id)
                 cr.execute('update leih_money_receipt set name=%s where id=%s', (mr_name, mr_id))
                 cr.commit()
-
-
             #### Create a challan
-
             picking_obj = self.pool.get('stock.picking')
             partner_obj = self.pool.get('res.partner')
             move_obj = self.pool.get('stock.move')
-
             for order in self.browse(cr, uid, ids, context=context):
-
-
                 picking_id = picking_obj.create(cr, uid, {
                     'origin': order.name,
                     'partner_id': False,
                     'date_done': stored_obj.date,
-                    'picking_type_id': 13, ## Hard Coded
+                    'picking_type_id': 13,  ## Hard Coded
                     # 'company_id': order.company_id.id,
                     'move_type': 'direct',
                     'note': "",
                     'invoice_state': 'none',
                 }, context=context)
                 self.write(cr, uid, [order.id], {'picking_id': picking_id}, context=context)
-                location_id = 25 # Source Location from where stock will reduce
-
-                destination_id = 9 ## Customer location
-
-
+                location_id = 25  # Source Location from where stock will reduce
+                destination_id = 9  ## Customer location
                 move_list = []
-
-
                 ## This is  for Fram3
                 if order.frame_id:
                     move_list.append(move_obj.create(cr, uid, {
@@ -153,25 +131,20 @@ class optics_sale(osv.osv):
                         'location_id': location_id,
                         'location_dest_id': destination_id,
                     }, context=context))
-
-
-
                 if order.hard_cover is True:
                     move_list.append(move_obj.create(cr, uid, {
                         'name': order.name,
                         'product_uom': 1,
-                        'product_uos': 1 ,
+                        'product_uos': 1,
                         'picking_id': picking_id,
                         'picking_type_id': 13,
-                        'product_id': 187, ## 187
+                        'product_id': 187,  ## 187
                         'product_uos_qty': abs(1),
                         'product_uom_qty': abs(1),
                         'state': 'draft',
                         'location_id': location_id,
                         'location_dest_id': destination_id,
                     }, context=context))
-
-
                 if order.cell_pad is True:
                     move_list.append(move_obj.create(cr, uid, {
                         'name': order.name,
@@ -179,16 +152,13 @@ class optics_sale(osv.osv):
                         'product_uos': 1,
                         'picking_id': picking_id,
                         'picking_type_id': 13,
-                        'product_id': 188, ## 188
+                        'product_id': 188,  ## 188
                         'product_uos_qty': abs(1),
                         'product_uom_qty': abs(1),
                         'state': 'draft',
                         'location_id': location_id,
                         'location_dest_id': destination_id,
                     }, context=context))
-
-
-
                 for opt_line in order.optics_lens_sale_line_id:
                     move_list.append(move_obj.create(cr, uid, {
                         'name': order.name,
@@ -196,33 +166,28 @@ class optics_sale(osv.osv):
                         'product_uos': 1,
                         'picking_id': picking_id,
                         'picking_type_id': 13,
-                        'product_id': 190, ## 190 This is  for lense product variant id
-                        'product_uos_qty': abs(opt_line.qty),
-                        'product_uom_qty': abs(opt_line.qty),
+                        'product_id': 190,  ## 190 This is  for lense product variant id
+                        'product_uos_qty': abs(1),
+                        'product_uom_qty': abs(1),
+                        # 'product_uos_qty': abs(opt_line.qty),
+                        # 'product_uom_qty': abs(opt_line.qty),
                         'state': 'draft',
                         'location_id': location_id,
                         'location_dest_id': destination_id,
                     }, context=context))
-
-
                 if picking_id:
                     picking_obj.action_confirm(cr, uid, [picking_id], context=context)
                     picking_obj.force_assign(cr, uid, [picking_id], context=context)
                     picking_obj.action_done(cr, uid, [picking_id], context=context)
-                    cr.execute("update optics_sale set delivery_id=%s where id=%s", (picking_id,ids[0]))
+                    cr.execute("update optics_sale set delivery_id=%s where id=%s", (picking_id, ids[0]))
                     cr.commit()
-
             ### Ends Here
-
             cr.execute("update optics_sale set state='confirmed' where id=%s", (ids))
             cr.commit()
         else:
             raise osv.except_osv(_('Warning!'),
                                  _('Minimum Payment is Required'))
-
         return self.pool['report'].get_action(cr, uid, ids, 'leih.report_optics_sale', context=context)
-
-
 
     def onchange_patient(self, cr, uid, ids, name, context=None):
         tests = {}
@@ -231,31 +196,24 @@ class optics_sale(osv.osv):
         tests['value'] = abc
         return tests
 
-
     def bill_cancel(self, cr, uid, ids, context=None):
         ## Bill Status Will Change
-
         cr.execute("update optics_sale set state='cancelled' where id=%s", (ids))
         cr.commit()
-
         return True
 
     def btn_pay_bill(self, cr, uid, ids, context=None):
         if not ids: return []
-
         inv = self.browse(cr, uid, ids[0], context=context)
         if inv.state == 'pending':
             raise osv.except_osv(_('Warning'), _('Please Confirm and Print the Optcs Form'))
-
         if inv.total == inv.paid:
             raise osv.except_osv(_('Full Paid'), _('Nothing to Pay Here. Already Full Paid'))
-
         dummy, view_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'leih',
                                                                              'optics_sale_payment_form_view')
         #
-
         # total=inv.total
-        
+
         return {
             'name': _("Pay Invoice"),
             'view_mode': 'form',
@@ -273,55 +231,49 @@ class optics_sale(osv.osv):
         }
         raise osv.except_osv(_('Error!'), _('There is no default company for the current user!'))
 
-
     def create(self, cr, uid, vals, context=None):
-
         f_prod_id = vals.get('frame_id')
         if f_prod_id:
-            has_qty=False
+            has_qty = False
             try:
                 # f_prod_id = vals.get('frame_id')
                 p_obj = self.pool['product.product'].browse(cr, uid, [f_prod_id], context=context)
-
                 if p_obj.qty_available > 0:
-                    has_qty=True
+                    has_qty = True
             except:
                 pass
-
             if has_qty == False:
                 raise osv.except_osv(_('Warning!'),
                                      _('Stock is not available'))
-
         if context is None:
             context = {}
-
         stored = super(optics_sale, self).create(cr, uid, vals, context)  # return ID int object
-
         if stored is not None:
             name_text = 'OPT- 0' + str(stored)
             cr.execute('update optics_sale set name=%s where id=%s', (name_text, stored))
             cr.commit()
-
         return stored
 
     def write(self, cr, uid, ids, vals, context=None):
         return super(optics_sale, self).write(cr, uid, ids, vals, context=context)
 
-
     @api.onchange('optics_lens_sale_line_id')
     def onchange_lens_bill(self):
         sumalltest = 0
-
         for item in self.optics_lens_sale_line_id:
             sumalltest = sumalltest + item.total_amount
-
         self.total = sumalltest + self.price
-
         self.due = sumalltest - self.paid + self.price
-
         return "X"
 
-
+    @api.onchange('frame_id')
+    def onchange_frame_bill(self):
+        frame_code=self.frame_id
+        self.price=frame_code.list_price
+        self.qty_available=frame_code.qty_available
+        return 'X'
+        # import pdb
+        # pdb.set_trace()
 
 
     @api.onchange('paid')
@@ -331,13 +283,11 @@ class optics_sale(osv.osv):
 
     @api.onchange('price')
     def onchange_price(self):
-        sumalltest=0
-
+        sumalltest = 0
         for item in self.optics_lens_sale_line_id:
             sumalltest = sumalltest + item.total_amount
-
         self.total = self.price + sumalltest
-        self.due =self.price + sumalltest - self.paid
+        self.due = self.price + sumalltest - self.paid
         return 'x'
 
     # @api.onchange('doctors_discounts')
@@ -348,7 +298,6 @@ class optics_sale(osv.osv):
     #     self.due = self.total - aft_discount - self.other_discount - self.paid
     #
     #     return "X"
-
     @api.onchange('other_discount')
     def onchange_other_discount(self):
         self.grand_total = self.total - self.after_discount - self.other_discount
@@ -389,57 +338,45 @@ class optics_sale(osv.osv):
 #         'commission_paid': fields.boolean("Commission Paid"),
 #
 #     }
-
-    # def onchange_test(self, cr, uid, ids, name, context=None):
-    #     tests = {'values': {}}
-    #     dep_object = self.pool.get('product.product').browse(cr, uid, name, context=None)
-    #
-    #     abc = {'price': dep_object.list_price, 'total_amount': dep_object.list_price,
-    #            'optics_sale_id.paid': dep_object.list_price}
-    #     tests['value'] = abc
-    #     # import pdb
-    #     # pdb.set_trace()
-    #     return tests
-
-    # def onchange_discount(self, cr, uid, ids, name, discount, context=None):
-    #     tests = {'values': {}}
-    #     dep_object = self.pool.get('examination.entry').browse(cr, uid, name, context=None)
-    #     abc = {'total_amount': round(dep_object.rate - (dep_object.rate * discount / 100))}
-    #     tests['value'] = abc
-    #     # import pdb
-    #     # pdb.set_trace()
-    #     return tests
-
-    # def create(self, cr, uid, vals, context=None):
-    #     # deliry_min_time
-    #     stored = super(optics_information, self).create(cr, uid, vals, context)
-    #
-    #
-    #     # today = datetime.datetime.strftime(datetime.datetime.today(), '%d/%m/%Y-%Hh/%Mm')
-    #
-    #     return 0
-
-    # def write(self, cr, uid, vals, context=None):
-    #     import pdb
-    #     pdb.set_trace()
-
-
-#starting the process of frame
-
+# def onchange_test(self, cr, uid, ids, name, context=None):
+#     tests = {'values': {}}
+#     dep_object = self.pool.get('product.product').browse(cr, uid, name, context=None)
+#
+#     abc = {'price': dep_object.list_price, 'total_amount': dep_object.list_price,
+#            'optics_sale_id.paid': dep_object.list_price}
+#     tests['value'] = abc
+#     # import pdb
+#     # pdb.set_trace()
+#     return tests
+# def onchange_discount(self, cr, uid, ids, name, discount, context=None):
+#     tests = {'values': {}}
+#     dep_object = self.pool.get('examination.entry').browse(cr, uid, name, context=None)
+#     abc = {'total_amount': round(dep_object.rate - (dep_object.rate * discount / 100))}
+#     tests['value'] = abc
+#     # import pdb
+#     # pdb.set_trace()
+#     return tests
+# def create(self, cr, uid, vals, context=None):
+#     # deliry_min_time
+#     stored = super(optics_information, self).create(cr, uid, vals, context)
+#
+#
+#     # today = datetime.datetime.strftime(datetime.datetime.today(), '%d/%m/%Y-%Hh/%Mm')
+#
+#     return 0
+# def write(self, cr, uid, vals, context=None):
+#     import pdb
+#     pdb.set_trace()
+# starting the process of frame
 class optics_lens_information(osv.osv):
     _name = 'optics.lens.sale.line'
-
-
     _columns = {
-
         'name': fields.many2one("product.lens", "Lens Name", ondelete='cascade'),
         'product_id': fields.many2one('product.product', "Lens Name"),
         'optics_sale_id': fields.many2one('optics.sale', "Information"),
         'price': fields.integer("Unit Price"),
         'qty': fields.integer("Quantity"),
         'total_amount': fields.integer("Total Amount"),
-
-
     }
 
     def onchange_lens(self, cr, uid, ids, name, context=None):
@@ -449,26 +386,25 @@ class optics_lens_information(osv.osv):
         #        'optics_sale_id.paid': dep_object.sell_price}
         # tests['value'] = abc
         #
-        
+
         return tests
 
-    def onchange_price(self, cr, uid, ids, qty,price, context=None):
+    def onchange_price(self, cr, uid, ids, qty, price, context=None):
         tests = {'values': {}}
         total_line = price * qty
         abc = {'qty': qty, 'total_amount': total_line}
         tests['value'] = abc
         return tests
 
-    def onchange_qty(self, cr, uid, ids, qty,price, context=None):
+    def onchange_qty(self, cr, uid, ids, qty, price, context=None):
         tests = {'values': {}}
-        
-        total_line=price*qty
-        abc = {'qty': qty,'total_amount':total_line}
+
+        total_line = price * qty
+        abc = {'qty': qty, 'total_amount': total_line}
         tests['value'] = abc
         #
-        
-        return tests
 
+        return tests
     # def onchange_discount(self, cr, uid, ids, name, discount, context=None):
     #     tests = {'values': {}}
     #     dep_object = self.pool.get('examination.entry').browse(cr, uid, name, context=None)
@@ -477,7 +413,6 @@ class optics_lens_information(osv.osv):
     #     # import pdb
     #     # pdb.set_trace()
     #     return tests
-
     # def create(self, cr, uid, vals, context=None):
     #     # deliry_min_time
     #     stored = super(optics_lens_information, self).create(cr, uid, vals, context)
@@ -492,21 +427,14 @@ class optics_lens_information(osv.osv):
     #     # today = datetime.datetime.strftime(datetime.datetime.today(), '%d/%m/%Y-%Hh/%Mm')
     #
     #     return 0
-
     # def write(self, cr, uid, vals, context=None):
     #     import pdb
     #     pdb.set_trace()
 
-#end of the process of lance
 
-
-
-
-
-
+# end of the process of lance
 class admission_payment_line(osv.osv):
     _name = 'optics.sale.payment.line'
-
     _columns = {
         'optics_sale_payment_line_id': fields.many2one('optics.sale', 'bill register payment'),
         'date': fields.datetime("Date"),
@@ -515,9 +443,4 @@ class admission_payment_line(osv.osv):
         'card_no': fields.char('Card Number'),
         'bank_name': fields.char('Bank Name'),
         'money_receipt_id': fields.many2one('leih.money.receipt', 'Money Receipt ID'),
-
     }
-
-
-
-
