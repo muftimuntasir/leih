4a0675035b7a1b09f56b01104308220e60db3a8e
kazi <kazialamin109@gmail.com>
2021-07-24 17:05:37 +0600

    Adding IR reports Journal ( Two Journals )

--- inventory/inventory_product_entry.py
+++ inventory/inventory_product_entry.py
@@ -10,25 +10,224 @@ class inventory_product_entry(osv.osv):
 
     _columns = {
 
-        'name': fields.char("Inventory Product entry"),
+        'name': fields.char("Entry No"),
         'invoice_no':fields.char("Invoice No"),
         'reference_no':fields.char("Reference No"),
+        'total':fields.float("Total Amount"),
         'partner_id':fields.many2one('res.partner','Partner Name'),
+        'grn_id':fields.many2one('stock.picking','GRN NO'),
+        'grn_journal_id':fields.many2one('account.move','GRN Journal'),
+        'advance_journal_id':fields.many2one('account.move','Advance Journal'),
         'department':fields.many2one("diagnosis.department","Department"),
         'warehouse_id': fields.many2one('stock.warehouse', 'Warehouse Location'),
         'inventory_product_entry_line_ids':fields.one2many('inventory.product.entry.line','inventory_product_entry_id',string="Inventory Requision Items"),
         'date':fields.date('Date'),
         'state': fields.selection(
-            [('pending', 'Pending'), ('confirmed', 'Confirmed'), ('cancelled', 'Cancelled')],
+            [('pending', 'Pending'), ('confirmed', 'Receive Product'),('verify', 'Verified'), ('cancelled', 'Cancelled')],
             'Status', default='pending', readonly=True)
 
     }
 
+    def confirm_finance(self, cr, uid, ids, context=None):
+        cc_ids = ids
+        for id in cc_ids:
+            ir_obj = self.browse(cr, uid, ids, context=context)
+            line_ids=[]
+            if ir_obj.state == 'verify':
+                raise osv.except_osv(_('Already Verified'), _('Sorry, it is already verified'))
 
-    def confirm_transfer(self):
+            if context is None: context = {}
+            if context.get('period_id', False):
+                return context.get('period_id')
+            periods = self.pool.get('account.period').find(cr, uid, context=context)
+            period_id = periods and periods[0] or False
+
+            if context is None:
+                context = {}
+
+            cc_obj = self.browse(cr, uid, ids, context=context)
+
+            line_ids.append([0, False, {
+                'name': cc_obj.name,
+                'partner_id': cc_obj.partner_id.id,
+                'account_id': cc_obj.partner_id.property_account_payable.id,
+                'debit': cc_obj.total,
+                'credit': 0,
+            }])
+
+            line_ids.append([0, False, {
+                'name': cc_obj.name,
+                'partner_id': cc_obj.partner_id.id,
+                'account_id': 6, ## Advance Cash
+                'debit': 0,
+                'credit': cc_obj.total,
+            }])
+
+            j_vals = {'name': '/',
+                      'journal_id': 6,  ## Advance Cash Journal
+                      'date': fields.date.today(),
+                      'period_id': period_id,
+                      'ref': cc_obj.name,
+                      'line_id': line_ids
+
+                      }
+            jv_entry = self.pool.get('account.move')
+
+            saved_jv_id = jv_entry.create(cr, uid, j_vals, context=context)
+            if saved_jv_id > 0:
+                journal_id = saved_jv_id
+            jv_entry.button_validate(cr, uid, [saved_jv_id], context)
+
+            ## Ends here
+
+            confirm_cash_collection_query = "UPDATE inventory_product_entry SET state='verify'," \
+                                            "advance_journal_id = {0}  WHERE id={1}".format(
+                saved_jv_id, id)
+            cr.execute(confirm_cash_collection_query)
+            cr.commit()
+
+        return True
 
 
-        return "X"
+
+    def confirm_transfer(self, cr, uid, ids, context=None):
+
+
+
+
+        cc_ids = ids
+        for id in cc_ids:
+            ir_obj = self.browse(cr, uid, ids, context=context)
+            if ir_obj.state == 'confirmed':
+                raise osv.except_osv(_('Already Confirmed'), _('Sorry, it is already confirmed'))
+
+            stock_picking_type_ids = self.pool['stock.picking.type'].search(cr, uid, [('warehouse_id', '=', ir_obj.warehouse_id.id),('code', '=', 'incoming')],
+                                                                            context=context)
+            stock_picking_type_data = self.pool['stock.picking.type'].browse(cr, uid, stock_picking_type_ids,
+                                                                             context=context)
+
+            sorce_id = None
+            dest_id = None
+            picking_type_id=None
+
+            for items in stock_picking_type_data:
+                sorce_id = items.default_location_src_id.id
+                dest_id = items.default_location_dest_id.id
+                picking_type_id = items.id
+            grn_vals = {
+                'partner_id': ir_obj.partner_id.id,
+                'date': fields.datetime.now(),
+                'origin': ir_obj.name,
+                'move_type': 'one',
+                'invoice_state': 'none',
+
+                'picking_type_id': picking_type_id,
+                # 'priority': 1, #Normal
+            }
+            ids = [id]
+
+
+            move_line = []
+            line_ids = []
+
+            for items in ir_obj.inventory_product_entry_line_ids:
+                move_line.append([0,False,{
+                    'product_id':items.product_name.id,
+                    'product_uom': 1,
+                    'product_uom_qty':items.quantity,
+                    'product_uos_qty': items.quantity,
+                    'name':ir_obj.name,
+                    'location_id':sorce_id,
+                    'location_dest_id':dest_id,
+                    'invoice_state':'none',
+                }])
+
+                line_ids.append([0, False, {
+                    'name': ir_obj.name,
+                    'partner_id': ir_obj.partner_id.id,
+                    'account_id': items.account_id.id,
+                    'debit': items.total_price,
+                    'credit': 0,
+                }])
+
+            grn_vals['move_lines']=move_line
+
+            grn_id = self.pool.get('stock.picking').create(cr, uid, grn_vals, context=context)
+
+            stock_picking_id = grn_id
+            stock_confirm = self.pool.get('stock.picking').action_confirm(cr, uid, [grn_id], context=context)
+
+            stock_picking = self.pool.get('stock.picking').do_enter_transfer_details(cr, uid, [stock_picking_id],
+                                                                                     context=context)
+
+            trans_obj = self.pool.get('stock.transfer_details')
+            trans_search = trans_obj.search(cr, uid, [('picking_id', '=', stock_picking_id)], context=context)
+
+            trans_search = [trans_search[len(trans_search) - 1]] if len(trans_search) > 1 else trans_search
+
+            trans_browse = self.pool.get('stock.transfer_details').browse(cr, uid, trans_search, context=context)
+
+            trans_browse.do_detailed_transfer()
+
+
+
+            ### Start Journal Creation fro Here
+
+            if context is None: context = {}
+            if context.get('period_id', False):
+                return context.get('period_id')
+            periods = self.pool.get('account.period').find(cr, uid, context=context)
+            period_id = periods and periods[0] or False
+
+            if context is None:
+                context = {}
+
+
+            cc_obj = self.browse(cr, uid, ids, context=context)
+
+
+
+            line_ids.append([0, False, {
+                'name': cc_obj.name,
+                'partner_id': cc_obj.partner_id.id,
+                'account_id': cc_obj.partner_id.property_account_payable.id,
+                'debit': 0,
+                'credit': cc_obj.total,
+            }])
+
+            j_vals = {'name': '/',
+                      'journal_id': 6,  ## Advance Cash Journal
+                      'date': fields.date.today(),
+                      'period_id': period_id,
+                      'ref': cc_obj.name,
+                      'line_id': line_ids
+
+                      }
+
+
+
+            jv_entry = self.pool.get('account.move')
+
+            saved_jv_id = jv_entry.create(cr, uid, j_vals, context=context)
+            if saved_jv_id > 0:
+                journal_id = saved_jv_id
+            jv_entry.button_validate(cr, uid, [saved_jv_id], context)
+
+            ## Ends here
+
+            confirm_cash_collection_query = "UPDATE inventory_product_entry SET state='confirmed'," \
+                                            "grn_id = {0}, grn_journal_id={1} WHERE id={2}".format(
+                grn_id,saved_jv_id, id)
+            cr.execute(confirm_cash_collection_query)
+            cr.commit()
+
+
+
+
+
+
+
+        return True
 
 
     def create(self, cr, uid, vals, context=None):
@@ -53,10 +252,27 @@ class inventory_product_entry_line(osv.osv):
     _name="inventory.product.entry.line"
 
 
+
+    def _default_account(self):
+        # XXX this gets the default account for the user's company,
+        # it should get the default account for the invoice's company
+        # however, the invoice's company does not reach this point
+        if self._context.get('type') in ('out_invoice', 'out_refund'):
+            return self.env['ir.property'].get('property_account_income_categ', 'product.category')
+        else:
+            return self.env['ir.property'].get('property_account_expense_categ', 'product.category')
+
+
+
     _columns = {
         'name':fields.char("Inventory Requisition Line Id"),
         'inventory_product_entry_id':fields.many2one("inventory.product.entry","Inventory Entry ID"),
         'product_name':fields.many2one('product.product','Product Name'),
+
+        'account_id' : fields.many2one('account.account', string='Account',
+                                     required=True, domain=[('type', 'not in', ['view', 'closed'])],
+                                     default=_default_account,
+                                     help="The income or expense account related to the selected product."),
         'quantity':fields.integer("Quantity"),
         'unit_price':fields.float("Unit Price"),
         'total_price':fields.float("Total Price")
